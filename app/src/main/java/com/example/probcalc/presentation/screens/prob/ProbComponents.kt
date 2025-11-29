0ddb3a623df82d9ca7a4bd3ff451dacc183a88fd
artur <arturaubdullin@gmail.com>
2025-11-28 19:18:43 -0800

    feat: add second screen UI
    -Add viewModel
    -Add components for second screen
    -Add ProbScreen

--- app/src/main/java/com/example/probcalc/presentation/screens/prob/ProbComponents.kt
+++ app/src/main/java/com/example/probcalc/presentation/screens/prob/ProbComponents.kt
@@ -1,2 +1,158 @@
 package com.example.probcalc.presentation.screens.prob
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun InputKTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text("Input k value") },
+        singleLine = true,
+        shape = RoundedCornerShape(20.dp)
+    )
+}
+@Composable
+fun InputNTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text("Input n value") },
+        singleLine = true,
+        shape = RoundedCornerShape(20.dp)
+    )
+}
+@Composable
+fun InputMTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text("Input m value") },
+        singleLine = true,
+        shape = RoundedCornerShape(20.dp)
+    )
+}
+@Composable
+fun InputRTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text("Input r value") },
+        singleLine = true,
+        shape = RoundedCornerShape(20.dp)
+    )
+}
+
+@Composable
+fun CalculateButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier.fillMaxWidth(),
+        onClick = onClick,
+        shape = RoundedCornerShape(20.dp)
+    ) {
+        Text(text = "Calculate")
+    }
+}
+
+
+@Composable
+fun AllMarked(
+    modifier: Modifier = Modifier,
+    onCheckedChange: (Boolean) -> Unit,
+    checked: Boolean
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+        Text(
+            text = "All marked"
+        )
+    }
+}
+
+@Composable
+fun Title(
+    modifier: Modifier = Modifier,
+    value: String
+) {
+    Text(
+        modifier = modifier
+            .fillMaxWidth(),
+        text = value,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+}
+
+@Composable
+fun ResultTitle(
+    modifier: Modifier = Modifier,
+    value: String
+) {
+    Text(
+        modifier = modifier
+            .fillMaxWidth(),
+        text = value,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun Result(
+    modifier: Modifier = Modifier,
+    value: String
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = { },
+        singleLine = true,
+        readOnly = true,
+        shape = RoundedCornerShape(20.dp)
+    )
+}
